c7dfe2f0dbcb170cd3f82063e96b44f5e144b236
AlexisBalzano <151195549+AlexisBalzano@users.noreply.github.com>
2026-08-27 23:11:04 +0200

    Bumped version to v1.1.0

--- vSMR/SMRPlugin.hpp
+++ vSMR/SMRPlugin.hpp
@@ -13,7 +13,7 @@
 #include "Logger.h"
 
 #define MY_PLUGIN_NAME      "vSMR"
-#define MY_PLUGIN_VERSION   "v1.0.9"
+#define MY_PLUGIN_VERSION   "v1.1.0"
 #define MY_PLUGIN_DEVELOPER "Pierre Ferran, Even Rognlien, Lionel Bischof, Daniel Lange, Juha Holopainen, Keanu Czirjak, French vACC"
 #define MY_PLUGIN_COPYRIGHT "GPL v3"
 #define MY_PLUGIN_VIEW_AVISO  "SMR radar display"
